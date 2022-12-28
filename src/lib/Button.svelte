f854e15cb52e4859f853266da64b2e6e45be7a25
Saurav Pal <resyfer.dev@gmail.com>
2022-12-28 23:53:13 +0530

    ADD: Everything

---
+++ src/lib/Button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let text: string;
+  export let href = "#";
+  export let className: string = "";
+</script>
+
+<a {href} class={className}>{text}</a>
+
+<style scoped>
+  a {
+    color: var(--theme-1-100);
+    transition: 0.2s ease-in-out;
+    text-decoration: none;
+    padding: 1.5vh 3vh;
+    border-radius: 0.75vh;
+    text-transform: uppercase;
+    border-bottom: 0.5vh solid var(--invisible);
+  }
+
+  a:hover {
+    background-color: var(--theme-3-100);
+    color: var(--theme-2-100);
+    border-bottom: 0.5vh solid var(--theme-2-100);
+  }
+</style>
---
+++ src/lib/Link.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte';
+
+  export let text: string;
+  export let href: string;
+</script>
+
+<a {href} target="_blank" rel="noreferrer">
+  {text} <Icon icon="ri:external-link-line" inline={true} />
+</a>
+
+<style>
+  a {
+    background-color: var(--theme-4-100);
+    color: var(--theme-1-100);
+    width: fit-content;
+    padding: 0.5vh 1vh;
+    border-radius: 10vh;
+    border: 0.2vh solid var(--invisible);
+    text-decoration: none;
+    transition: 0.25s ease-in-out;
+    margin: 0.5vh;
+  }
+
+  a:hover {
+    color: var(--theme-2-100);
+    background-color: var(--theme-3-100);
+    border: 0.2vh solid var(--theme-2-100);
+  }
+</style>
